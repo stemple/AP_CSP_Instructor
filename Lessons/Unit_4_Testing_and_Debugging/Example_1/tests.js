7e6247e39c4148442b43696d14266a8e1b2ab236
stemple <stemples@gmail.com>
2016-08-22 14:51:01 -0700

    Update to example 1 in Unit_4

--- Lessons/Unit_4_Testing_and_Debugging/Example_1/tests.js
+++ Lessons/Unit_4_Testing_and_Debugging/Example_1/tests.js
@@ -5,3 +5,9 @@ QUnit.test( "Test the returnFive function", function( assert ) {
 QUnit.test( "Test the returnFive function 2", function( assert ) {
       assert.notEqual( returnFive(), 4, "We expect the value 5." );
     });
+
+QUnit.test( "Test the iSort function", function( assert ) {
+    var x = Array(3,2,1);
+    iSort(x, 3);
+    assert.deepEqual(x, Array(1,2,3), "We expect the value [1,2,3]." );
+    });
