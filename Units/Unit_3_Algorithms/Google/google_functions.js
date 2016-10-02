3a0c5ea35312e09cda297fc57e42eb727430e509
stemple <stemples@gmail.com>
2016-10-01 19:35:26 -0700

    New Google Functions lesson.

---
+++ Units/Unit_3_Algorithms/Google/google_functions.js
@@ -0,0 +1,11 @@
+/**
+ * Multiplies the input value by 2.
+ *
+ * @param {number} input The value to multiply.
+ * @return The input multiplied by 2.
+ * @customfunction
+ */
+function DOUBLE(input) {
+  return input * 2;
+}
+
