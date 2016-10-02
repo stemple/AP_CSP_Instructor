0fbf26f0cc96034ce3956c2afe2a2cdbdb03a018
stemple <stemples@gmail.com>
2016-10-01 19:36:40 -0700

    Added new function HYPOTENUSE

--- Units/Unit_3_Algorithms/Google/google_functions.js
+++ Units/Unit_3_Algorithms/Google/google_functions.js
@@ -9,3 +9,15 @@ function DOUBLE(input) {
   return input * 2;
 }
 
+/**
+ * Uses the pythagorean formula to calculate the hypotenuse
+ *
+ * @param {number} a The length of one leg.
+ * @param {number} b The length of the other leg.
+ * @return The hypotenuse length
+ * @customfunction
+ */
+function HYPOTENUSE(a, b){
+    var c = Math.pow((Math.pow(a,2) + Math.pow(b,2)), .5);
+    return c;
+}
