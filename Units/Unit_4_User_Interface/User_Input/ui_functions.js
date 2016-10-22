79fa202322fe7a775bd457efd0fb56df9fe52b3b
stemple <stemples@gmail.com>
2016-10-22 16:13:24 -0700

    Updated the User Input lesson

---
+++ Units/Unit_4_User_Interface/User_Input/ui_functions.js
@@ -0,0 +1,38 @@
+function randomGreeting(){
+    var random = Math.floor(Math.random()*4);
+    switch (random) {
+        case 0:
+            return "Hola!";
+        case 1:
+            return "Bonjour";
+        case 2:
+            return "こんにちは";
+        default:
+            return "Howdy";
+    }
+}
+
+function checkPrime(number) {
+    var stop = Math.floor(number / 2);
+    for (i = 2; i <= stop; ++i) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function displayPrimeTest(outputId, inputId) {
+    var input = document.getElementById(inputId).value;
+    var outputText = "";
+    if (checkPrime(input) == true) {
+        outputText = "The number " + input + " is a prime number.";
+    } else {
+        outputText = "The number " + input + " is not prime number.";
+    }
+    document.getElementById(outputId).innerHTML = outputText;
+}
+
+function displayGreeting(id) {
+    document.getElementById(id).innerHTML = randomGreeting();
+}
