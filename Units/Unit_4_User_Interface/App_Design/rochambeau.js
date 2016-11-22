ddce0c002d2724ec45be40aff0cdec9262dc5318
stemple <stemples@gmail.com>
2016-11-21 17:19:40 -0800

    Game is complete and ready!

--- Units/Unit_4_User_Interface/App_Design/rochambeau.js
+++ Units/Unit_4_User_Interface/App_Design/rochambeau.js
@@ -45,15 +45,15 @@ function displayGameResult(resultId){
     if (result == 0) {
         // Display that it was a win
         document.getElementById(resultId).innerHTML = message + "YOU WIN! " + score;
-        document.getElementById(resultId).className = "alert alert-info";
+        document.getElementById(resultId).className = "alert alert-success";
     } else if (result == 2) {
         // Display that it was a loss
         document.getElementById(resultId).innerHTML = message + "YOU LOOSE! " + score;
-        document.getElementById(resultId).className = "alert alert-warning";
+        document.getElementById(resultId).className = "alert alert-danger";
     } else {
         // Display that it was a tie
         document.getElementById(resultId).innerHTML = message + "A tie. " + score;
-        document.getElementById(resultId).className = "alert alert-warning";
+        document.getElementById(resultId).className = "alert alert-info";
     }
 }
 
