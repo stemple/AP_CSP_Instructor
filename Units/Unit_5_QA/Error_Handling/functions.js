21a417cfdce098f5ba396ec4f6d3746536a19f38
stemple <stemples@gmail.com>
2017-01-30 21:53:53 -0800

    New error handling tutorial code.

--- Units/Unit_5_QA/Error_Handling/functions.js
+++ Units/Unit_5_QA/Error_Handling/functions.js
@@ -1,55 +1,80 @@
 /**
-* This function removes part of string between two sub strings
-* @param {string} text This is the original string
-* @param {string} start This is the starting string
-* @param {string} end This is the ending string
-* @return {string} The string in between
-*/
+ * Removes part of string between two sub strings
+ * @param {string} text The original string
+ * @param {string} start The starting string
+ * @param {string} end The ending string
+ * @return {string} The string in between
+ */
 function between(string, start, end) {
     var startAt = string.indexOf(start);
-
     if (startAt == -1) {
+        //return undefined;
         throw new Error("No start found: " + start);
     }
-
     startAt += start.length;
     var endAt = string.indexOf(end, startAt);
-
     if (endAt == -1) {
+        //return undefined;
         throw new Error("No end found: " + end);
     }
-
     return string.slice(startAt, endAt);
 }
-
 /**
-* This function validates a phone number
-* @param {string} value This is the phone number
-* @return {string} The string in between
-*/
-function validPhone(phoneNum) {
-    var areaCode = between(phoneNum, "(", ")");
-    var baseNum
-    if (phoneNum.charAt(3) == "-" &&
-//            nums[i].length == 8 &&
-//            Number(nums[i].slice(0, 3))
-//            && Number(nums[i].slice(4))) {
-//            validNums.push(nums[i]);
-//        }
-//    }
-    return phoneNum;
-}
-
-
-function validate(phoneInputId, outputId) {
-
-    var output = document.getElementById(outputId);
+ * Returns an area code from a phone number: (###) ###-####
+ * @param   {string} phoneNum The phone number
+ * @returns {string} The area code
+ * @throws {Error} If the format is incorrect
+ */
+function getAreaCode(phoneNum) {
+    var areaCode;
     try {
-        var phoneNum = document.getElementById(phoneInputId).value;
-        validPhone(phoneNum);
-        output.innerHTML = "Yea! Valid."
-    } catch (err) {
-        output.innerHTML = err.message;
+        areaCode = between(phoneNum, "(", ")");
+        areaCode = areaCode.trim();
+        if (areaCode.length == 3 && Number(areaCode)) {
+            return areaCode;
+        }
+        else {
+            throw new Error("Invalid area code: " + areaCode);
+        }
     }
-
+    catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+/**
+ * Returns the CO code from a phone number.
+ * @param   {string} phoneNum The input phone number: (###) ###-####
+ * @returns {string} The CO code.
+ */
+function getCoCode(phoneNum) {
+    var coCode = between(phoneNum, ")", "-").trim();
+    return coCode;
+}
+/**
+ * Returns the line code a phone number.
+ * @param   {string} phoneNum The input phone number: (###) ###-####
+ * @returns {string} The line code
+ */
+function getLineCode(phoneNum) {
+    var lineCode = phoneNum.slice(phoneNum.indexOf("-") + 1, phoneNum.length).trim();
+    return lineCode;
+}
+/**
+ * Displays the area code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayAreaCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+    // Now try to get the code
+    try {
+        var areaCode = getAreaCode(phoneNum);
+        outputText = "Your area code is " + areaCode;
+    }
+    catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+    document.getElementById(outputId).innerHTML = outputText;
 }
--- Units/Unit_5_QA/Error_Handling/tests.js
+++ Units/Unit_5_QA/Error_Handling/tests.js
@@ -1,22 +1,27 @@
-QUnit.test("Test the validatePhone function.", function (assert) {
+QUnit.test("Test the getAreaCode function.", function (assert) {
     var num = "(415) 444-4444";
-    var result = validPhone(num);
-    assert.deepEqual(num, result, "(415) 444-4444 is valid");
-    var num = "(415)444-4444";
-    var result = validPhone(num);
-    assert.deepEqual(num, result, "(415)444-4444 is valid");
+    var result = getAreaCode(num);
+    assert.deepEqual("415", result, "Valid area code test passed.");
 });
 
-QUnit.test( "Errors thrown for validPhone", function( assert ) {
+QUnit.test("Test the getCoCode function.", function (assert) {
+    var num = "(415) 444-4444";
+    var result = getCoCode(num);
+    assert.deepEqual("444", result, "Returned a valid central office code.");
+});
+
+QUnit.test("Test the getLineCode function.", function (assert) {
+    var num = "(415) 444-4444";
+    var result = getLineCode(num);
+    assert.deepEqual(result, "4444", "Returned a valid line code.");
+});
+
+QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
     assert.throws( function() {
-        validPhone("415)444-5555");
+        getAreCode("415)444-5555");
     }, "Missing '('. An error should have been thrown." );
 
     assert.throws( function() {
-        validPhone("(415 444-5555");
+        getAreCode("(415 444-5555");
     }, "Missing ')'. An error should have been thrown." );
-
-    assert.throws( function() {
-        validPhone("(415) 44-5555");
-    }, "(###) ##-#### is invalid. An error should have been thrown." );
 });
