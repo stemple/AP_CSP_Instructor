67164b843e53f538842ab704acbdec891a97dfe0
stemple <stemples@gmail.com>
2017-02-01 07:27:16 -0800

    Some final touches to the Exception Handling lesson files

--- Units/Unit_5_QA/Error_Handling/functions.js
+++ Units/Unit_5_QA/Error_Handling/functions.js
@@ -19,6 +19,7 @@ function between(string, start, end) {
     }
     return string.slice(startAt, endAt);
 }
+
 /**
  * Returns an area code from a phone number: (###) ###-####
  * @param   {string} phoneNum The phone number
@@ -41,6 +42,7 @@ function getAreaCode(phoneNum) {
         throw new Error("Invalid phone number: " + error.message);
     }
 }
+
 /**
  * Returns the CO code from a phone number.
  * @param   {string} phoneNum The input phone number: (###) ###-####
@@ -48,8 +50,12 @@ function getAreaCode(phoneNum) {
  */
 function getCoCode(phoneNum) {
     var coCode = between(phoneNum, ")", "-").trim();
+    if(coCode.length != 3 || !Number(coCode)) {
+        throw new Error("Invalid Co Code: " + coCode);
+    }
     return coCode;
 }
+
 /**
  * Returns the line code a phone number.
  * @param   {string} phoneNum The input phone number: (###) ###-####
@@ -57,8 +63,12 @@ function getCoCode(phoneNum) {
  */
 function getLineCode(phoneNum) {
     var lineCode = phoneNum.slice(phoneNum.indexOf("-") + 1, phoneNum.length).trim();
+    if(lineCode.length != 4 || !Number(lineCode)) {
+        throw new Error("Invalid Line Code: " + lineCode);
+    }
     return lineCode;
 }
+
 /**
  * Displays the area code for an inputted phone number
  * @param {string} inputId  The element id for the text box
@@ -71,6 +81,46 @@ function displayAreaCode(inputId, outputId) {
     try {
         var areaCode = getAreaCode(phoneNum);
         outputText = "Your area code is " + areaCode;
+    }
+    catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+    document.getElementById(outputId).innerHTML = outputText;
+}
+
+/**
+ * Displays the CO code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayCoCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+    // Now try to get the code
+    try {
+        var coCode = getCoCode(phoneNum);
+        outputText = "Your co code is " + coCode;
+    }
+    catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+    document.getElementById(outputId).innerHTML = outputText;
+}
+
+/**
+ * Displays the line code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayLineCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+    // Now try to get the code
+    try {
+        var lineCode = getLineCode(phoneNum);
+        outputText = "Your line code is " + lineCode;
     }
     catch (error) {
         console.log(error.message);
