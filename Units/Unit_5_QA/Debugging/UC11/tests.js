aa25830ec37796fccf9f4d5fbfca5429807949ea
stemple <stemples@gmail.com>
2017-02-04 18:16:04 -0800

    Moved the solution files for UC11 into a new folder called UC11_Solutions.

--- Units/Unit_5_QA/Debugging/UC11/tests.js
+++ Units/Unit_5_QA/Debugging/UC11/tests.js
@@ -12,37 +12,3 @@
 //USER STORY 5
 
 //USER STORY 6
-
-QUnit.test("Test the multTable function.", function (assert) {
-    var row = 2;
-    var col = 1;
-    var result = multTable(row, col);
-    assert.deepEqual(result, "<table><tr><td>1</td></tr><tr><td>2</td></tr></table>", "We expect the result to be 1, 2.")
-});
-
-QUnit.test("Test the oddFilter function.", function (assert) {
-    var a = [3, 5, 1, 6, 2, 4];
-    var result = oddFilter(a);
-    assert.deepEqual(result, [3, 5, 1], "We expect result to be 3, 5, 1");
-    a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    result = oddFilter(a);
-    assert.deepEqual(result, [1, 3, 5, 7, 9, 11], "We expect result to be 1, 3, 5, 7, 9, 11");
-});
-
-QUnit.test("Test the sort function.", function (assert) {
-    var a = [3, 5, 1, 6, 2, 4];
-    var result = sort(a);
-    assert.deepEqual(result, [1, 2, 3, 4, 5, 6], "We expect result to be 1,2,3,4,5,6.");
-});
-
-QUnit.test("Test the listTriangular function.", function (assert) {
-    var a = 3
-    var result = listTriangularNumbers(a);
-    assert.deepEqual(result, "", "We expect result to be 1,2,3,4,5,6.");
-});
-
-QUnit.test("Test the sort function.", function (assert) {
-    var a = [3, 5, 1, 6, 2, 4];
-    var result = sort(a);
-    assert.deepEqual(result, [1, 2, 3, 4, 5, 6], "We expect result to be 1,2,3,4,5,6.");
-});
