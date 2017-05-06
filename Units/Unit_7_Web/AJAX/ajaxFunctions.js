da8462229da5917276f8606dcedd94deda468a6d
stemple <stemples@gmail.com>
2017-05-06 16:57:52 -0700

    Some changes to the AJAX lesson. More to come...

--- Units/Unit_7_Web/AJAX/ajaxFunctions.js
+++ Units/Unit_7_Web/AJAX/ajaxFunctions.js
@@ -1,11 +1,22 @@
 function loadTextDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("output").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "ajax_info.txt", true);
-  xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("output1").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+}
+
+function getJSONData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("output2").innerHTML = this.responseText;
+        }
+    };
+    // This get JSON data from a online database of movies!
+    xhttp.open("GET", "http://www.omdbapi.com/?t=star+wars", true);
+    xhttp.send();
 }
