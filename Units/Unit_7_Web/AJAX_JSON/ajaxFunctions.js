2fef33f4f41be85123b374134ed436cac6bcb70b
stemple <stemples@gmail.com>
2017-05-23 07:46:37 -0700

    Made some file name changes and UI improvements on the AJAX and JSON tutorial.

--- Units/Unit_7_Web/AJAX_JSON/ajaxFunctions.js
+++ Units/Unit_7_Web/AJAX_JSON/ajaxFunctions.js
@@ -2,8 +2,10 @@
  * Uses AJAX to query an internet data source for zip codes
  * @param {string} zipId The element id that has the zip code
  */
-function getPlace(zipId) {
+function findZip(zipId) {
+    // First get the zip code from the HTML textbox
     var zip = document.getElementById(zipId).value;
+    // Now make a HTTP request
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
@@ -21,6 +23,7 @@ function getPlace(zipId) {
             // Waiting for a response...
         }
     };
+    // Notice how the URL is appended with the zip code
     var url = "http://api.zippopotam.us/us/" + zip;
     httpRequest.open("GET", url, true);
     httpRequest.send();
